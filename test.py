96361d2d6606341ba249cd61abaaf9d8f97bcd3a
minsangkim <kmswin1@korea.ac.kr>
2019-04-29 14:41:04 +0900

    add test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import pickle
+import os
+
+if __name__=='_main_':
+    print (os.getcwd())
+    f = open('blog_spam.txt', 'r')
+    lex = []
+    while True:
+        data = f.readline()
+        data = data.split('\n')
+        data = data[0].split('\t')
+        if (data[0] == 'tr'):
+            if (data[1] == '정상'):
+                for i in range(3, len(data)):
+                    if data[i] not in self.lex:
+                        lex.append(data[i])
+                    else:
+                        for i in range(3, len(data)):
+                            if data[i] not in self.lex:
+                                lex.append(data[i])
+        if not data: break
+    f.close()
+    with open("result.txt",'wb') as f:
+        pickle.dump(lex, f)
